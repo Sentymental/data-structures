867e03855dd66f7357f840bcac91a963f8aacfb0
Sentymental <luk.szwajnoch95@gmail.com>
2022-04-25 12:19:24 +0200

    Added, left and right append and append before and after target value

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,42 +1,124 @@
 """
 LinkedList implementation in Python 3.10
+
+Node: (the first node is called: head)
+- data: data it stored in Node
+- next_node: contains the reference to next node
+
+LinkedLists are used as: queues/stacks/graphs
+
 """
+from typing import Any
 
 
+# Create a Node:
 class Node:
-    def __init__(self, data, prev_node=None, next_node=None):
+    """ 
+    Class that creates each node
+    - data: data that is stored in Node
+    - next_node: reference to the next node
+    """
+    def __init__(self, data: Any, next_node: Any = None):
         self.data = data
-        self.prev_node = prev_node
-        self.next_ndoe = next_node
+        self.next = next_node
 
     def __repr__(self):
         return self.data
 
 
+# Create a LinkedList:
 class LinkedList:
-    def __init__(self, data=None):
+    """ 
+    Implementing a Linked Lists
+    - nodes: list of predefined values to be treated as Node
+    """
+    def __init__(self, nodes = None):
         self.head = None
-        self.tail = None
-        if data is not None:
-            self.add_multiple_nodes(data)
+        if nodes is not None:
+            node = Node(data=nodes.pop(0))
+            self.head = node
+            for i in nodes:
+                node.next = Node(data=i)
+                node = node.next
 
-    def __repr__(self) -> str:
-        return " -> ".join(str(node) for node in self)
+
+    def add_left(self, node):
+        node.next = self.head
+        self.head = node
 
 
-    def __len__(self):
-        count = 0
-        node = self.head
-        while node:
-            count += 1
-            node = node.next
+    def add_right(self, node):
+        if self.head is None:
+            self.head = node
+            return
+        for current_node in self:
+            pass
+        current_node.next = node
 
-        return count
+
+    def add_after(self, target, new_node):
+        if self.head is None:
+            raise Exception("LinkedList is empty.")
+
+        for node in self:
+            if node.data == target:
+                new_node.next = node.next
+                node.next = new_node
+                return
+
+        raise Exception(f"Node with data {target} not found")
+
+
+    def add_before(self, target, new_node):
+        if self.head is None:
+            raise Exception("LinkedList is empty.")
+
+        if self.head.data == target:
+            return self.add_left(new_node)
+
+        prev_node = self.head
+        for node in self:
+            if node.data == target:
+                prev_node.next = new_node
+                new_node.next = node
+                return
+            prev_node = node
+
+        raise Exception(f"Node with data {target} not found")
 
 
     def __iter__(self):
-        current = self.head
-        while current:
-            yield current
-            current = current.next
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
 
+
+    def __repr__(self) -> str:
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+
+        nodes.append("None")
+        return " -> ".join(nodes)
+
+
+
+
+
+# Drivers:
+node_list = ["b", "c", "d"]
+linked_list = LinkedList(node_list)
+print("Original LinkedList: ", linked_list)
+
+#for node in LinkedList(["a", "b", "c"]):
+#    print(node)
+
+
+linked_list.add_left(Node("a"))
+print("Added left: ", linked_list)
+
+linked_list.add_right(Node("e"))
+print("Added right:", linked_list)
