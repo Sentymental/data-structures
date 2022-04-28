5fc6e2fc72309edf8219f4cb7cd856797261de74
Sentymental <luk.szwajnoch95@gmail.com>
2022-04-28 15:41:41 +0200

    Added sum_two on non sorted list

--- leet_code/arrays/sum_two.py
+++ leet_code/arrays/sum_two.py
@@ -1,41 +1,29 @@
 """
-Given an array of sorted integers nums and an integer target,
+Given an array of integers nums and an integer target,
 return indices of the two numbers such that they add up to target.
-You may assume that each input would have exactly
-one solution, and you may not use the same element twice.
-You can return the answer in any order.
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice. You can return the
+answer in any order.
 """
 
-# Test Cases:
-arr = [2, 7, 11, 15]
-arr1 = [2, 3, 4]
-arr2 = [1, 3, 4, 5, 7, 10, 11]
 
+# Test cases:
+arr = [2, 1, 5, 3]
 
-# Main Function
-def sum_two(arr: list[int], target: int) -> list[int]:
-    """
-    Sum Two function returns indexes of two
-    intergers that sums to the target
-    """
-    start, end = 0, len(arr) - 1
+def sum_two(nums: list[int], target: int) -> list[int]:
+    """ Sum two numbers from list and reach the target """
 
-    while start < end:
-        result = arr[start] + arr[end]
+    hashmap = dict()
 
-        if result > target:
-            end -= 1
-
-        elif result < target:
-            start += 1
-
+    for i, v in enumerate(nums):
+        result = target - v
+        if result in hashmap:
+            return [hashmap[result], i]
         else:
-            return [start, end]
+            hashmap[v] = i
 
-    return []
+    return
 
 
 # Tests:
-assert sum_two(arr, 9) == [0, 1], "Wrong"
-assert sum_two(arr1, 6) == [0, 2], "Wrong"
-assert sum_two(arr2, 9) == [2, 3], "Wrong"
+assert sum_two(arr, 4) == [1, 3], "Wrong Answer"
