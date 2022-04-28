7ce1e9d31c935bea056301e9a554d6d771cdefbc
Sentymental <luk.szwajnoch95@gmail.com>
2022-04-28 19:10:57 +0200

    Add test to sum_two.py

--- leet_code/arrays/duplicates.py
+++ leet_code/arrays/duplicates.py
@@ -11,24 +11,25 @@ Hashset:
 
 # Main function:
 def contains_duplicate(nums: list[int]) -> bool:
-    """ Check if in list are duplicated ints. """
+    """Check if in list are duplicated ints."""
 
     # Check if list is not empty:
     if not len(nums):
         return False
 
-    hashset = set()
+    hashset: set = set()
 
     for i in nums:
         if i in hashset:
             return True
         hashset.add(i)
+
     return False
 
 
 # Tests:
-assert contains_duplicate([2,14,18,22,22]) == True, "Wrong Answer"
-assert contains_duplicate([1,2,3,1]) == True, "Wrong Answer"
-assert contains_duplicate([1,2,3,4]) == False, "Wrong Answer"
-assert contains_duplicate([1,1,1,3,3,4,3,2,4,2]) == True, "Wrong Answer"
+assert contains_duplicate([2, 14, 18, 22, 22]) == True, "Wrong Answer"
+assert contains_duplicate([1, 2, 3, 1]) == True, "Wrong Answer"
+assert contains_duplicate([1, 2, 3, 4]) == False, "Wrong Answer"
+assert contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]) == True, "Wrong Answer"
 assert contains_duplicate([]) == False, "Wrong Answer"
--- leet_code/arrays/sum_two.py
+++ leet_code/arrays/sum_two.py
@@ -6,24 +6,29 @@ and you may not use the same element twice. You can return the
 answer in any order.
 """
 
+# Main Function:
+def sum_two(nums: list[int], target: int):
+    """Sum two numbers from list and reach the target"""
 
-# Test cases:
-arr = [2, 1, 5, 3]
+    if not len(nums) or target == 0:
+        return False
 
-def sum_two(nums: list[int], target: int) -> list[int]:
-    """ Sum two numbers from list and reach the target """
-
-    hashmap = dict()
+    hashmap: dict = dict()
 
     for i, v in enumerate(nums):
         result = target - v
+
         if result in hashmap:
             return [hashmap[result], i]
+
         else:
             hashmap[v] = i
 
-    return
+    return False
 
 
 # Tests:
-assert sum_two(arr, 4) == [1, 3], "Wrong Answer"
+assert sum_two([2, 1, 5, 3], 4) == [1, 3], "Wrong Answer"
+assert sum_two([], 123) == False, "Wrong Answer"
+assert sum_two([], 0) == False, "Wrong Answer"
+assert sum_two([2, 1, 5, 3], 123) == False, "Wrong Answer"
